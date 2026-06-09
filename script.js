62587bc72731e3ce369f2caf30fa3342d0e7cabd
taydevgit <taydevgaming@gmail.com>
2026-06-08 23:47:26 -0500

    added mp4 and updated svg for icons in list. added more formatting to the flexbox. added javascript file to reduce playback speed of the mp4 gif. git push

---
+++ script.js
@@ -0,0 +1,7 @@
+// Target the video element using its CSS class name
+const spaceVideo = document.querySelector('.space-bg-video');
+
+// Set the playback rate (0.5 makes it slower)
+if (spaceVideo) {
+    spaceVideo.playbackRate = 0.5;
+}
